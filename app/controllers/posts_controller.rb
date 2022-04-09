342c131acea9b96a67674e71ab28d74adcdc500c
kk <kazuhiro.2308.a@gmail.com>
2022-04-09 04:33:46 +0000

    Add user microposts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
   def index
-    # @postsに代入されている配列をPost.allに書き換えてください
-    @posts = Post.all
+    # Post.allにorderメソッドを用いて、新しい投稿が上から順に表示されるようにしてください
+    @posts = Post.all.order(created_at: :desc)
+  end
+  
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def new
+  end
+  
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get "posts/index" => "posts#index"
-
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  # データを送信するためのルーティングを追加してください
+  post "posts/create" => "posts#create"
+  
   get "/" => "home#top"
   get "about" => "home#about"
 end
