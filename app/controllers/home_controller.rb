1a2f8cf84c245cf7b220fd2c6fd73088019698a5
kk <kazuhiro.2308.a@gmail.com>
2022-04-09 14:18:53 +0000

    Add user microposts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
+  # before_actionにforbid_login_userメソッドを指定してください
+  before_action :forbid_login_user, {only: [:top]}
+
   def top
   end
   
-  # aboutアクションを追加してください
   def about
   end
-  
-
 end
