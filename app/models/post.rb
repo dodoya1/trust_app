d3d8e97a6490b3b513898e69f10802d4a833484a
kk <kazuhiro.2308.a@gmail.com>
2022-04-10 03:52:08 +0000

    Add user microposts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
-  # contentカラムに対して、文字数を制限するためのバリデーションを追加してください
   validates :content, {presence: true, length: {maximum: 140}}
+  validates :user_id, {presence: true}
+  
+  # インスタンスメソッドuserを定義してください
+  def user
+    return User.find_by(id: self.user_id)
+  end
+  
 end
